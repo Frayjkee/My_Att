2e61f312243dc3fc526938d1e19a6e8405d2073e
Ilya Maksimov <iluxamax@yandex.ru>
2024-04-23 17:46:44 +0300

    Добавление вывода результата

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ def filter_short_strings(arr):
             result.append(string)
     return result
 def main():
-    
+
     # Ввод массива строк с клавиатуры
     arr = input("Введите элементы массива через пробел: ").split()
 
      # Формирование нового массива из строк длиной не более 3 символов
     filtered_arr = filter_short_strings(arr)
+
+ # Вывод результата
+    print("Результат:")
+    print(filtered_arr)
