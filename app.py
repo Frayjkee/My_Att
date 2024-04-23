e7a184a249bf0a3e25aaa4e36c650a293b19470f
Ilya Maksimov <iluxamax@yandex.ru>
2024-04-23 17:43:03 +0300

    Создал функция для ввода массива через пробел

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ def filter_short_strings(arr):
         if len(string) <= 3:
             result.append(string)
     return result
+def main():
+    # Ввод массива строк с клавиатуры
+    arr = input("Введите элементы массива через пробел: ").split()
